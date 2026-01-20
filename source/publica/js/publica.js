ca0540a48ccd93e8a8d157ad47f659c9a4ff8c77
Oriol Noya <uriyeahh@gmail.com>
2026-01-20 13:58:57 +0100

    canvis

---
+++ source/publica/js/publica.js
@@ -0,0 +1,7 @@
+
+// @prepros-prepend "template/jquery.min.js"
+// @prepros-prepend "template/bootstrap.bundle.min.js"
+// @prepros-prepend "template/plugins.js"
+// @prepros-prepend "template/jquery.ajaxchimp.min.js"
+// @prepros-prepend "template/contact-form.js"
+// @prepros-prepend "template/all.js" 
